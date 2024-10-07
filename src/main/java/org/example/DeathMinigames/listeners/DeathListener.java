16cbcdee810aaab4c446c2765c81aa133f430045
Luca Weimer <luca.weimer.42@gmail.com>
2024-10-07 17:51:17 +0200

    added a settings menu from where all config data can be changed; every config topic is saved in an own HashMap, every time something is changed in config it will be changed in both the Hashmap and the file, so data wont be lost when the server crashes, the HashMaps get the data from the config when Plugin is enabled

--- src/main/java/org/example/DeathMinigames/listeners/DeathListener.java
+++ src/main/java/org/example/DeathMinigames/listeners/DeathListener.java
@@ -24,21 +24,24 @@ public class DeathListener implements Listener {
 
     @EventHandler
     public void onDeath(PlayerDeathEvent event) {
+        Main main = new Main();
+        Introduction introduction = new Introduction();
+
         Player player = event.getPlayer();
         Inventory inventory = Bukkit.createInventory(null, 45);
         inventory.setContents(event.getPlayer().getInventory().getContents());
         Location deathpoint = event.getPlayer().getLocation();
 
         deaths.put(player.getUniqueId(), deathpoint);
-        Main.getPlugin().getLogger().info("Player got new deathpoint");
+        main.getPlugin().getLogger().info("Player got new deathpoint");
         if (inventory.isEmpty()) {
-            if(Main.checkIfPlayerActivatedPlugin(player)) {
+            if(main.checkConfigBoolean(player, "UsesPlugin")) {
                 player.sendActionBar(Component.text("Inventar wurde nicht gespeichert, da es leer war")
                         .color(NamedTextColor.GOLD)
                         .decoration(TextDecoration.ITALIC, true));
             }
         } else if (!inventories.containsKey(player.getUniqueId())){
-            if(Main.checkIfPlayerActivatedPlugin(player)) {
+            if(main.checkConfigBoolean(player, "UsesPlugin")) {
                 player.sendActionBar(Component.text("Inventar wurde gespeichert")
                         .color(NamedTextColor.GOLD)
                         .decoration(TextDecoration.ITALIC, true));
@@ -46,12 +49,12 @@ public class DeathListener implements Listener {
             inventories.put(player.getUniqueId(), inventory);
         }
 
-        if(!Main.checkIfPlayerActivatedPlugin(player)) {
+        if(!main.checkConfigBoolean(player, "UsesPlugin")) {
             dropInv(player);
         }
     }
 
-    private static void dropInv(Player player) {
+    private void dropInv(Player player) {
         for(int i = 0; i < inventories.get(player.getUniqueId()).getSize(); i++) {
             if(inventories.get(player.getUniqueId()).getItem(i) == null) continue;
             assert inventories.get(player.getUniqueId()).getItem(i) != null;
